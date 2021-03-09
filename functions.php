0f9c7a7f536719c187aebc9645f001f6b2c2c2c7
Sadat Hossen Himel <sadat.himel@gmail.com>
2021-03-09 13:51:07 +0600

    custom background support

--- functions.php
+++ functions.php
@@ -25,6 +25,9 @@ function alpha_bootstrapping(){
         "height" => "100"
     );
     add_theme_support( "custom-logo", $alpha_custom_logo_defaults );
+
+    add_theme_support("custom-background");
+
     register_nav_menu( "topmenu", __("Top menu", "alpha"));
     register_nav_menu( "footermenu", __("Footer menu", "alpha"));
 }
