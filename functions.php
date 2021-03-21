c621b522a13ee649a1f7e8edffe4a3a5753bff57
Sadat Hossen Himel <sadat.himel@gmail.com>
2021-03-21 18:57:36 +0600

    search result highlight

--- functions.php
+++ functions.php
@@ -174,3 +174,15 @@ function alpha_post_class($classes){
     return $classes;
 }
 add_filter("post_class", "alpha_post_class");
+
+
+function alpha_highlight_search_results($text){
+    if(is_search()){
+        $pattern = '/('. join('|', explode(' ', get_search_query())).')/i';
+        $text = preg_replace($pattern, '<span class="search-highlight">\0</span>', $text);
+    }
+    return $text;
+}
+add_filter('the_content', 'alpha_highlight_search_results');
+add_filter('the_excerpt', 'alpha_highlight_search_results');
+add_filter('the_title', 'alpha_highlight_search_results');
