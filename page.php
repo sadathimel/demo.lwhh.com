a6870efdb41949077784da2f2e97005725528159
Sadat Hossen Himel <sadat.himel@gmail.com>
2021-03-21 17:03:12 +0600

    one page plugin uses multiple pages

--- page.php
+++ page.php
@@ -1,6 +1,55 @@
 <?php get_header(); ?>
 <body <?php body_class(); ?>>
 <?php get_template_part( "/template-parts/common/hero" ); ?>
+
+                <div class="container">
+                    <?php 
+                        if (is_front_page()) {
+                    ?>
+                        <div class="row">
+                            <div class="col-md-8 offset-md-2">
+
+                                <?php 
+                                $attachments = new Attachments( 'testimonials',30 );
+                                    if (class_exists("Attachments") && $attachments->exist()) {
+                                ?>
+                                <h2 class="text-center">
+                                    <?php _e("Testimonials","alpha"); ?>
+                                </h2>
+                                <?php } ?>
+
+                                <div class="testimonials slider text-center">
+                                <?php
+                                if (class_exists("Attachments")) {
+                                    
+                                    $attachments = new Attachments( 'testimonials' );
+                                    if ( $attachments->exist() ) {
+                                        while ( $attachment = $attachments->get() ) { ?>
+                                            <div>
+                                                <?php echo $attachments->image( 'thumbnail' ); ?>
+                                                <h4><?php echo esc_html( $attachments->field( 'name' )); ?> </h4>
+                                                <p><?php echo esc_html( $attachments->field( 'testimonial' )); ?></p>
+                                                <p>
+                                                    <?php echo esc_html($attachments->field( 'position' )); ?>
+                                                    <strong>
+                                                        <?php echo esc_html( $attachments->field( 'company')); ?>
+                                                    </strong>
+                                                        
+                                                </p>
+                                            </div>
+                                            <?php
+                                        }
+                                    }
+                                }
+                                
+                                ?>
+                                </div>
+                            </div>
+                        </div>
+                    <?php
+                        }
+                    ?>
+                </div>
     
                 <div class="posts">
 
@@ -32,10 +81,10 @@
                                                 $thumbnail_url = get_the_post_thumbnail_url( null, "large" );
                                                 echo '<a class="popup" href="#" data-featherlight="image">';
                                                 // printf('<a href="%s" data-featherlight="myimage.png">',$thumbnail_url);
-                                                the_post_thumbnail("large", array("class"=>"img-fluid") );
+                                                the_post_thumbnail("large", array("class"=>"img-fluid"));
                                                 echo '</a>';
                                             }
-                                                            the_content( );
+                                            the_content();
 
                                             next_post_link();
 
