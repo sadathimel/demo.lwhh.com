4767c35c5bc843924b520922f13882c4700082da
Sadat Hossen Himel <sadat.himel@gmail.com>
2021-03-25 16:24:06 +0600

    Update comments.php

--- comments.php
+++ comments.php
@@ -2,7 +2,7 @@
 //Get only the approved comments
 $args = array(
     'status' => 'approve',
-    'post_id' => get_the_ID();
+    'post_id' => get_the_ID()
 );
  
 // The comment Query
@@ -12,8 +12,30 @@ $comments = $comments_query->query( $args );
 // Comment Loop
 if ( $comments ) {
  foreach ( $comments as $comment ) {
-  echo '<p>' . $comment->comment_content . '</p>';
- }
+ ?>
+ <div class="container">
+	<div class="media">
+	  <?php echo get_avatar($comment,64,null,null,array(
+	  		'class'=>'mr-3'
+	  ));?>
+	  <div class="media-body">
+	    <h5 class="mt-0">
+	    	<?php comment_author($comment); ?>
+	    	<?php edit_comment_link(); ?>
+	    </h5>
+	    <?php 
+	    	comment_text($comment);
+	    ?>
+	  </div>
+	</div>
+ <?php
+ 	}
 } else {
  echo 'No comments found.';
 }
+?>
+
+	<div class="comment-form">
+		<?php comment_form( ); ?>
+	</div>
+</div>
