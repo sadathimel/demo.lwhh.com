436e53ecf18886ae23eac9afc6f285d8e2a29530
Sadat Hossen Himel <sadat.himel@gmail.com>
2021-04-08 10:52:38 +0600

    update

---
+++ cq-wp.php
@@ -0,0 +1,49 @@
+<?php 
+/*
+*Template Name: Custom Query WPQuery
+*/ 
+?>
+<?php
+get_header();
+?>
+<body <?php body_class(); ?>>
+<?php get_template_part("/template-parts/common/hero");?>
+<div class="posts text-center">
+	<?php
+    $paged = get_query_var("paged") ? get_query_var("paged") : 1;
+    $posts_ids = array(15,18,9,2,80);
+    $posts_per_page = 3;
+    $total = 9;
+    $_p = new WP_Query( array( 
+        'category_name'  => 'new',
+        'posts_per_page' => $posts_per_page,
+        'paged'          => $paged 
+    ) );
+		while ($_p -> have_posts()) {
+			$_p -> the_post()
+			?>
+            <h2><a href="<?php the_permalink( ); ?>"><?php the_title(); ?></h2></a>
+            <?php
+		}
+        wp_reset_query();
+	 ?>
+
+        <div class="container post-pagination">
+            <div class="row">
+                <div class="col-md-12">
+                    <?php 
+                        echo paginate_links([
+                            'current'   => $paged,
+                            'total'     => $_p->max_num_pages,
+                            'prev_next' => false
+                        ])
+                     ?>
+                </div>                
+            </div>
+        </div>
+    </div>
+
+</div>
+<?php
+get_footer();
+?>
--- cq.php
+++ cq.php
@@ -11,9 +11,15 @@ get_header();
 <div class="posts text-center">
 	<?php
     $paged = get_query_var("paged") ? get_query_var("paged") : 1;
+
+    $posts_ids = array(15,18,9,2,80);
+    $posts_per_page = 2;
+    $total = 9;
     $_p = get_posts(array(
-        'posts_per_page' => 2,
-        'post__in'       => array(15,18,9,2,80),
+        'posts_per_page' => $posts_per_page,
+        // 'post__in'       => $posts_ids,
+        'author__in'     => [1],
+        'number_post'    => $total,
         'orderby'        => 'post__in',
         'paged'          => $paged
     ));
@@ -31,11 +37,9 @@ get_header();
             <div class="col-md-4"> </div>
             <div class="col-md-8">
                 <?php
-                the_posts_pagination( array(
-                    "screen_reader_text"=>' ',
-                    "prev_text" => "New Posts",
-                    "next_text" => "Old Posts"
-                ));
+                    echo paginate_links( array(
+                         'total' => ceil($total/$posts_per_page)
+                    ));
                 ?>
             </div>
         </div>
