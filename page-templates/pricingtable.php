cb91f17cc2958a4281c13180c2f4b79a2716335a
Sadat Hossen Himel <sadat.himel@gmail.com>
2021-04-20 17:50:02 +0600

    update

--- page-templates/pricingtable.php
+++ page-templates/pricingtable.php
@@ -8,18 +8,34 @@ get_header();
 	$pricing = get_post_meta(get_the_ID(), "_alpha_pt_pricing_table",true);
 ?>
 	
-  <?php 
-  	print_r($pricing);
-   ?>
 
    <div class="container">
    	<div class="row">
-   		
+   		<?php 
+   			foreach ($pricing as $ptc) :
+   		?>
+
+   		<div class="col-md-4">
+   			<h2><?php echo esc_html($ptc['_alpha_pt_priceing_caption']) ?></h2>
+   			<ul>
+   				<?php 
+   					foreach ($ptc['_alpha_pt_pricing_option'] as $pto) :
+   				?>
+   				<li><?php echo esc_html($pto); ?></li>
+   				<?php
+   				endforeach;
+   				?>
+   			</ul>
+   			<h3><?php echo esc_html($ptc['_alpha_pt_Price']); ?></h3>
+   		</div>
+
+   		<?php
+   			endforeach;
+   		?>
    	</div>
    </div>
 
 <?php 
 get_footer();
 
-?>
 
