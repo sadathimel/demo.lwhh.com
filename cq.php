773f1b42459f3011f6f58431409fee138961f633
Mahadi001 <sadat.himel@gmail.com>
2021-09-09 15:52:46 +0600

    update

--- cq.php
+++ cq.php
@@ -1,37 +1,29 @@
 <?php
-get_header();
+    /**
+     * Template Name: Custom Query
+     */
+
+    get_header();
 ?>
-<body <?php body_class(); ?>>
-<?php get_template_part("/template-parts/common/hero");?>
+<body<?php body_class();?>>
+<?php get_template_part( "/template-parts/common/hero" );?>
 <div class="posts text-center">
-    <h1>
-        Posts Under <?php single_tag_title(); ?>
-    </h1>
+
 	<?php
-		while (have_posts()) {
-			the_post();
-			?>
-            <h2><a href="<?php the_permalink( ); ?>"><?php the_title(); ?></h2></a>
+        $_p = get_posts( [
+            'post__in' => [ 484, 643, 526 ],
+			'orderby'=> 'post__in'
+        ] );
+
+        foreach ( $_p as $post ) {
+            setup_postdata( $post );
+        ?>
+            <h2><a href="<?php the_permalink();?>"><?php the_title();?></h2></a>
             <?php
-		}
-	 ?>
+                }
+            ?>
 
-
-    <div class="container mb-4">
-    	<div class="row">
-    		<div class="col-md-4"></div>
-    		<div class="col-md-8">
-    			<?php
-    				the_posts_pagination(array(
-    					"screen_reader_text"=>' ',
-    					"prev_text" => "New Posts",
-    					"next_text" => "Old Post"
-    				));
-    			 ?>
-    		</div>
-    	</div>
-    </div>
 </div>
 <?php
-get_footer();
+    get_footer();
 ?>
